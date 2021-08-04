2a91a65433671ad97f6651daab429dd7eb50148e
Mauricio <mauri.yapura@yahoo.com>
2021-08-03 22:05:31 -0300

    some fixes

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const PORT = process.env.PORT || "3000";
+//const cors = require("cors")
+
+const app = express();
+
+//app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/", (req,res)=>{
+    res.sendFile(__dirname + "public/index.html");
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Server listening on port ${PORT}`);
+})
+
+
+
+
+
+
+
+
+
+
